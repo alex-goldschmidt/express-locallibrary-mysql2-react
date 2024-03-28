7b935b0b38eacdda66fb5b8a864d41c078fc4d25
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2024-03-27 21:48:57 -0400

    feat: set up react app, express app, and db

---
+++ server/db.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql2");
+var dotenv = require("dotenv");
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_ROOT,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    dateStrings: true,
+  })
+  .promise();
+
+pool.getConnection((error, connection) => {
+  if (error instanceof Error) throw error;
+  console.log("Successfully connected to the database.");
+  connection.release();
+});
+
+module.exports = pool;
